ab113619d47e113f4babb6d84680a965992efb90
utazaev <utazaev@gmail.com>
2024-03-11 23:48:14 +0300

    добавил time.sleep(5)

--- loader.py
+++ loader.py
@@ -1,7 +1,9 @@
 from aiogram import types, Dispatcher, Bot
 from aiogram.enums import ParseMode
 from data import config
+import time
 
+time.sleep(5)
 bot = Bot(token=config.BOT_TOKEN, parse_mode=ParseMode.HTML)
 
 dp = Dispatcher()
